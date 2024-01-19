ec66dfc2c33ef54d9a06af215453c33fa25ec4ac
Eleni <57360206+elenisal94@users.noreply.github.com>
2024-01-19 12:55:23 +0000

    Layout boilerplate setup
    Installed ejs-mate and created boilerplate document, applying the boiler plate on all views pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Recipe = require('./models/recipe');
 const app = express();
@@ -18,6 +19,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
