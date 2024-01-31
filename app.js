773ad0120ac555dc99f57e966f6223150857e990
Eleni <57360206+elenisal94@users.noreply.github.com>
2024-01-31 17:23:25 +0000

    Serving Static Assets
    Created a public directory to host any static assets such as javascripts or stylesheets.

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/recipes', recipes)
 app.use('/recipes/:id/reviews', reviews)
