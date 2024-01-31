04c349aa06901c708bda9a8b8b78a55649c021ab
Eleni <57360206+elenisal94@users.noreply.github.com>
2024-01-31 17:50:18 +0000

    Session Configuration
    Downloaded and set-up express-session.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const ExpressError = require('./helper/ExpressError')
+const session = require('express-session');
+const ExpressError = require('./helper/ExpressError');
 const methodOverride = require('method-override');
 const app = express();
 
@@ -25,6 +26,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+const sessionConfig = {
+    secret: 'somethingsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 app.use('/recipes', recipes)
 app.use('/recipes/:id/reviews', reviews)
