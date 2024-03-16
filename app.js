3f03da74845c9bc85661cf014204ebe09a9fbd20
Eleni <57360206+elenisal94@users.noreply.github.com>
2024-03-16 17:34:56 +0000

    Security
    Enhanced website security by adding express-mongo-sanitize and helmet, sanitising HMTL through Joi and slightly adjusting the app's Session/Cookies.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
+require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
@@ -12,9 +13,10 @@ const ExpressError = require('./helper/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+const helmet = require('helmet');
 const app = express();
-
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const recipeRoutes = require('./routes/recipes');
@@ -36,18 +38,73 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+    mongoSanitize({
+        replaceWith: '_',
+    }),
+);
 const sessionConfig = {
+    name: 'session',
     secret: 'somethingsecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://cdn.jsdelivr.net/npm/mapboxgl-spiderifier@1.0.10/index.min.js",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://cdn.jsdelivr.net",
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: false,
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dr35pjw8f/",
+                "https://images.unsplash.com",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -57,6 +114,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
--- schemas.js
+++ schemas.js
@@ -1,11 +1,34 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 // Define ingredient schema
 const ingredientSchema = Joi.object({
     amount: Joi.number().min(0).allow(null, ''),
     measurementUnit: Joi.string(),
     measurementShorthand: Joi.string(),
-    ingredientName: Joi.string().required(),
+    ingredientName: Joi.string().required().escapeHTML(),
 });
 
 // Extend ingredient schema to enforce amount requirement based on measurementUnit
@@ -20,15 +43,15 @@ const ingredientWithAmountSchema = ingredientSchema.keys({
 // Define recipe schema
 module.exports.recipeSchema = Joi.object({
     recipe: Joi.object({
-        title: Joi.string().max(150).required(),
-        description: Joi.string().max(1400).required(),
+        title: Joi.string().max(150).required().escapeHTML(),
+        description: Joi.string().max(1400).required().escapeHTML(),
         prepHours: Joi.number().min(0).allow(null, ''),
         prepMinutes: Joi.number().min(0).allow(null, ''),
         serves: Joi.number().required().min(1),
         countryCode: Joi.string().required(),
         measurementSystem: Joi.string().required(),
         ingredients: Joi.array().items(ingredientWithAmountSchema).required(),
-        method: Joi.array().items(Joi.string()).required(),
+        method: Joi.array().items(Joi.string().escapeHTML()).required(),
     }).required().custom((value, helpers) => {
         const { prepHours, prepMinutes } = value;
         if (!prepHours && !prepMinutes) {
@@ -36,8 +59,8 @@ module.exports.recipeSchema = Joi.object({
         }
         return value;
     }),
-    altText: Joi.array().items(Joi.string().max(300)),
-    newAltText: Joi.array().items(Joi.string().max(300)),
+    altText: Joi.array().items(Joi.string().max(300).escapeHTML()),
+    newAltText: Joi.array().items(Joi.string().max(300).escapeHTML()),
     deleteImages: Joi.array()
 });
 
@@ -45,6 +68,6 @@ module.exports.recipeSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().max(600)
+        body: Joi.string().required().max(600).escapeHTML(),
     }).required()
 });
